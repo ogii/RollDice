3a00520e2b5758ab790807e1f7172cca8dc557d4
ogii <ordinallx@gmail.com>
2017-05-12 03:36:27 +0000

    Updated challenge.js comments

--- challenges.js
+++ challenges.js
@@ -15,7 +15,7 @@ var lastDice;
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
-        // 1. Random number
+        // 1. Determine Random number
         var dice1 = Math.floor(Math.random() * 6) + 1;
         var dice2 = Math.floor(Math.random() * 6) + 1;
 
